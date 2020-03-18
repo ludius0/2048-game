4636dedaf9977d7a028ee3deb15995d7e24832b1
ludius <57571014+ludius0@users.noreply.github.com>
2020-03-18 16:27:43 +0100

    Add files via upload

---
+++ 2048/without gui/main.py
@@ -0,0 +1,48 @@
+import logic as l
+
+
+### GENERATE BOARD AND FIRST NUMBER ###
+print("Welcome in game 2048!\nPlayable moves:\n'a' -> LEFT\n'd' -> RIGHT\n'w' -> UP\n's' -> DOWN\n" + "-"*20)
+board = [[0 for i in range(4)] for i in range(4)]
+board = l.generate_number(board)
+l.p_board(board)
+
+
+### GAME LOOP
+while l.game:
+    if input() == "a":      # LEFT
+        board = l.compress(board)
+        board = l.merge(board)
+        board = l.compress(board)
+        board = l.generate_number(board)
+        l.p_board(board)
+    elif input() == "d":    # RIGHT
+        board = l.reverse(board)
+        board = l.compress(board)
+        board = l.merge(board)
+        board = l.compress(board)
+        board = l.reverse(board)
+        board = l.generate_number(board)
+        l.p_board(board)
+    elif input() == "w":    # UP
+        board = l.transp(board)
+        board = l.compress(board)
+        board = l.merge(board)
+        board = l.compress(board)
+        board = l.transp(board)
+        board = l.generate_number(board)
+        l.p_board(board)
+    elif input() == "s":    # DOWN
+        board = l.transp(board)
+        board = l.reverse(board)
+        board = l.compress(board)
+        board = l.merge(board)
+        board = l.compress(board)
+        board = l.reverse(board)
+        board = l.transp(board)
+        board = l.generate_number(board)
+        l.p_board(board)
+    elif input() == "help":
+        print("Welcome in game 2048!\nPlayable moves:\n'a' -> LEFT\n'd' -> RIGHT\n'w' -> UP\n's' -> DOWN")
+    else:
+        print("Error in input...\nTry again.")
