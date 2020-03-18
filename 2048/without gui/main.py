bdfb22f5a7f40bee1e8697eb01a25202b1ddd2bc
ludius <57571014+ludius0@users.noreply.github.com>
2020-03-18 16:32:47 +0100

    Update main.py

--- 2048/without gui/main.py
+++ 2048/without gui/main.py
@@ -10,13 +10,14 @@ l.p_board(board)
 
 ### GAME LOOP
 while l.game:
-    if input() == "a":      # LEFT
+    user_input = input()
+    if user_input == "a":      # LEFT
         board = l.compress(board)
         board = l.merge(board)
         board = l.compress(board)
         board = l.generate_number(board)
         l.p_board(board)
-    elif input() == "d":    # RIGHT
+    elif user_input == "d":    # RIGHT
         board = l.reverse(board)
         board = l.compress(board)
         board = l.merge(board)
@@ -24,7 +25,7 @@ while l.game:
         board = l.reverse(board)
         board = l.generate_number(board)
         l.p_board(board)
-    elif input() == "w":    # UP
+    elif user_input == "w":    # UP
         board = l.transp(board)
         board = l.compress(board)
         board = l.merge(board)
@@ -32,7 +33,7 @@ while l.game:
         board = l.transp(board)
         board = l.generate_number(board)
         l.p_board(board)
-    elif input() == "s":    # DOWN
+    elif user_input == "s":    # DOWN
         board = l.transp(board)
         board = l.reverse(board)
         board = l.compress(board)
@@ -42,7 +43,7 @@ while l.game:
         board = l.transp(board)
         board = l.generate_number(board)
         l.p_board(board)
-    elif input() == "help":
+    elif user_input == "help":
         print("Welcome in game 2048!\nPlayable moves:\n'a' -> LEFT\n'd' -> RIGHT\n'w' -> UP\n's' -> DOWN")
     else:
         print("Error in input...\nTry again.")
